eb5f58fa70e3bc6364247bc60c6cf8cdb6c7547a
Linimik <jameskimimk@gmail.com>
2022-02-20 14:32:27 -0500

    Initial repo

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<main>
+	<h1 class="bg-black text-2xl text-red-50">If this is black tailwind works!</h1>
+</main>
+
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
